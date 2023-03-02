7537ba3336c650046441ecada732607f89243ae0
juan mendoza <j.mendoza712@gmail.com>
2023-03-02 15:45:26 +0000

    event listener for clicking on a square

--- src/index.js
+++ src/index.js
@@ -6,31 +6,49 @@ import './index.css';
 // Every custome-made componet class inherits from React.Component class
 
 class Square extends React.Component {
-  // Equivalent to Ruby's Initialize method. It's used for declaring a component "state"
-  constructor(props) {
-    super(props); // All JS classes' constructor should start with this line -> Inheritance
-    this.state = {
-      value: null,
-    };
-  }
+  // Constructor is gone since Square no longer keeps track of its own state
 
   render() {
     return (
       // Function syntax inside onClick is called Arrow Function.
       <button
       className="square"
-      onClick={ () => this.setState({ value: 'X' }) } // this.setState is setting the value of "value"
+      onClick={ () => this.props.onClick() } // Will tell Board when Square is clicked. This is an "event listener"
       >
-        {this.state.value} {/* this.state.value is how a component state is called */}
+        {this.props.value} {/* Will display the "value" prop passed when Square is rendered */}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  // Equivalent to Ruby's Initialize method. It's used for pre-setting a component "state"
+  // This will set the state of the game by telling each Square its own state (null, X or O)
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  // Event handler -> event = click
+  // Will update this.state.squares[i] where "i" is the Square that was clicked
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+    // console.log (this.state.squares)
+  }
+
   renderSquare(i) {
-    // "value" is what's called a "prop" (property)
-    return <Square value={i}/>; // <Square /> refers to the Square class declared above
+    // "value" and "onClick" is what's called a "prop" (property)
+    // <Square /> refers to the Square class declared above
+    return (
+      <Square
+        value={ this.state.squares[i] } // Each Square will receive a value prop that will either be 'X', 'O', or null.
+        onClick = { () => this.handleClick(i) } // "Event handler". Will allow communication btw Board and Square when Square is clicked
+      />
+    )
   }
 
   render() {
